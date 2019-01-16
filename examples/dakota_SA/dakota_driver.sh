708d6a44e05317d115f5a6a77da424cd6e67a0d7
Sessim <msessim@202-132-227-10.unnamed.mse.ufl.edu>
2019-01-16 17:40:06 -0500

    reorganize and add dakota SA files

---
+++ examples/dakota_SA/dakota_driver.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+# --------------
+# PRE-PROCESSING
+# --------------
+# Incorporate the parameters from DAKOTA into the template
+
+# getting workdir tag
+name=${PWD##*/}
+
+export num=`echo $name | cut -c 9-`
+
+# copy any necessary files and data files into workdir
+cd ../
+cp mosdak_2D.template workdir.$num/
+#cp mesh_file.e workdir.$num/
+cp dprepro workdir.$num/
+cp mosdak_2D.i workdir.$num/
+
+# RUN the simulation from workdir.num
+cd workdir.$num/
+#input params.in into the moose simulation
+./dprepro --left-delimiter=% --right-delimiter=% params.in mosdak_2D.template mosdak_2D.i
+
+# --------
+# ANALYSIS
+# --------
+#
+mpirun -np 8 ../../picachu-opt -i mosdak_2D.i > temp.out
+
+# ---------------
+# POST-PROCESSING
+# ---------------
+# extract function value from the simulation output - Attention with the order!!
+# 1 line for each response function
+#for testing
+#tail -n+2 moose_out.csv|cut -f3 -d','
+
+tail -1 mosdak_2D_out.csv|cut -f1 -d',' >> results.tmp
+tail -1 mosdak_2D_out.csv|cut -f2 -d',' >> results.tmp
+
+# write results.out and cleanup
+cp results.tmp ./results.out
+cd ../
